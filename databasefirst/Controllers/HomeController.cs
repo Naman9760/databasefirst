b8738b7148cd0bdc93a4633a0e46b57fa2e481d0
User <User@DESKTOP-3571300>
2022-05-14 10:19:43 +0530

    Add project files.

---
+++ databasefirst/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using databasefirst.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace databasefirst.Controllers
+{
+    public class HomeController : Controller
+    {
+        Emp1Entities Db = new Emp1Entities();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            var data = Db.Employes.ToList();
+            return View(data);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Employe emp)
+        {
+            Db.Employes.Add(emp);
+            Db.SaveChanges();
+            return RedirectToAction("index");
+        }
+    }
+}
